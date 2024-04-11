fa25c7db5268e3d26864da09a044c4b3f39372cf
WooHyuck Han <vue-cli@kakao.com>
2024-04-11 18:18:40 +0900

    feat: 회원가입 로그인 페이지 구현 (#6)
    * refactor: Updated favicon image to Logo
    
    * refactor: Replaced the original image with modified Image
    
    * refactor: Add page Route and Feature Reset CSS
    
    * feat: Add Login and SignUp Form components with SCSS styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from './routes/login/LoginPage'
+import SignUpPage from './routes/signup/SignUpPage'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
       </Routes>
     </>
   )
