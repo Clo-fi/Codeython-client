2bf48ae5f37f63c01f7c8c6f77f08fc2b30ad604
minji <73338341+metamoong@users.noreply.github.com>
2024-04-12 15:22:20 +0900

    Feat/home (#8)
    * ♻️  refactor: 공통 버튼 일반 속성 추가
    
    * ♻️  refactor: prettier 검사 제거
    
    * ✨ feat: 홈 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { Route, Routes } from 'react-router-dom'
-import LoginPage from './routes/login/LoginPage'
-import SignUpPage from './routes/signup/SignUpPage'
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./routes/login/LoginPage";
+import SignUpPage from "./routes/signup/SignUpPage";
+import HomePage from "./routes/home/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
