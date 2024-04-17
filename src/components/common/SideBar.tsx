bdeca723d0668784d03c5d7b98db823c250fe34e
KoreanCow <vue-cli@kakao.com>
2024-04-17 12:38:44 +0900

    ♻️  refactor: 안쓰는 import  React 삭제

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 import styles from "./SideBar.module.scss";
 import { useLocation, useNavigate } from "react-router-dom";
