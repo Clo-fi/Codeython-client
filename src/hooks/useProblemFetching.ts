01788adf1f04ef65c08509e95ac541d23e1fea35
KoreanCow <vue-cli@kakao.com>
2024-04-21 16:48:47 +0900

    ✨ feat: 문제 상세조회 훅 생성

---
+++ src/hooks/useProblemFetching.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import instance from '../api/axios';
+
+interface ProblemInfo {
+  title: string;
+  content: string;
+  limitFactors: { factor: string }[];
+  limitTime: number;
+  baseCode: { language: string; code: string }[];
+  testcases: {
+    inputCase: string[][];
+    outputCase: string[][];
+    description: string;
+  };
+  difficulty: number;
+}
+
+const useProblemFetching = (problemId: string) => {
+  const [problemInfo, setProblemInfo] = useState<ProblemInfo | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchProblemInfo = async () => {
+      try {
+        const response = await instance.get(`/problem/${problemId}`);
+        setProblemInfo(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching problem info: ', error);
+        // setTimeout(() => {
+        //   fetchProblemInfo();
+        // }, 5000);
+      }
+    };
+
+    fetchProblemInfo();
+
+    // Cleanup function to reset state when unmounting or when problemId changes
+    return () => {
+      setProblemInfo(null);
+      setIsLoading(true);
+    };
+  }, [problemId]);
+
+  return { problemInfo, isLoading };
+};
+
+export default useProblemFetching;
