6e2b2d762b78ab27407b74736beba63f84e31a78
KoreanCow <vue-cli@kakao.com>
2024-04-22 15:21:29 +0900

    ♻️ refactor: 테스트 코드 주석처리

--- src/routes/playMulti/PlayMultiPage.tsx
+++ src/routes/playMulti/PlayMultiPage.tsx
@@ -24,31 +24,31 @@ const PlayMultiPage = () => {
   const { problemInfo, isLoading } = useProblemFetching(problemId!);
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const exampleInfo: ProblemInfo = {
-    "title": "Add Two Numbers",
-    "content": "You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.",
-    "limitFactors": [
-      { "factor": "The number of nodes in each linked list is in the range [1, 100]." },
-      { "factor": "0 <= Node.val <= 9" }
-    ],
-    "limitTime": 3,
-    "baseCode": [
-      {
-        "language": "java",
-        "code": "class Main {\n  public static void Main() {\n  }\n}"
-      },
-      {
-        "language": "javascript",
-        "code": "class Solution {\n  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {\n  }\n}"
-      }
-    ],
-    "testcases": {
-      "inputCase": [["1,2"], ["3,4"], ["5,6"]],
-      "outputCase": [["3,4"], ["5,6"], ["7,8"]],
-      "description": "this is description"
-    },
-    "difficulty": 1
-  };
+  // const exampleInfo: ProblemInfo = {
+  //   "title": "Add Two Numbers",
+  //   "content": "You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.",
+  //   "limitFactors": [
+  //     { "factor": "The number of nodes in each linked list is in the range [1, 100]." },
+  //     { "factor": "0 <= Node.val <= 9" }
+  //   ],
+  //   "limitTime": 3,
+  //   "baseCode": [
+  //     {
+  //       "language": "java",
+  //       "code": "class Main {\n  public static void Main() {\n  }\n}"
+  //     },
+  //     {
+  //       "language": "javascript",
+  //       "code": "class Solution {\n  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {\n  }\n}"
+  //     }
+  //   ],
+  //   "testcases": {
+  //     "inputCase": [["1,2"], ["3,4"], ["5,6"]],
+  //     "outputCase": [["3,4"], ["5,6"], ["7,8"]],
+  //     "description": "this is description"
+  //   },
+  //   "difficulty": 1
+  // };
 
 
   return (
