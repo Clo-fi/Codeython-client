408c9222d0c28396994797c5ca4b1c13d2359193
minji <73338341+metamoong@users.noreply.github.com>
2024-04-25 17:21:27 +0900

    feat: 요청 수정 사항 반 (#81)
    * ♻️ refactor: 수정 요청 사항
    
    * 🐛 fix: search param이 변경 되어도 unsubscribe 방지

--- src/api/problem/problem.ts
+++ src/api/problem/problem.ts
@@ -5,3 +5,12 @@ export const getProblemList = async (): Promise<GetProblemsRequest> => {
   const { data } = await instance.get("problems");
   return data;
 };
+
+export const patchSelectedProblem = async (
+  roomId: number,
+  problemId: number
+): Promise<void> => {
+  return instance.patch(`rooms/${roomId}`, {
+    problemId,
+  });
+};
